f28b32779d44c64703b713975afca012518a4292
enriquepaez <enrique.paezpozo@gmail.com>
2024-09-06 16:48:22 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+//* ELEMENTOS PRINCIPALES DEL DOM
+
+// pantallas
+const splashScreenNode = document.querySelector("#splash-screen")
+const gameScreenNode = document.querySelector("#game-screen")
+const gameOverScreenNode = document.querySelector("#game-over-screen")
+
+// botones
+const startBtnNode = document.querySelector("#start-btn")
+
+// game box
+const gameBoxNode = document.querySelector("#game-box")
+
+
+//* VARIABLES GLOBALES DEL JUEGO
+
+
+
+//* FUNCIONES GLOBALES DEL JUEGO
+
+
+
+//* EVENT LISTENERS
+
+
+
+
+
+
+
+
+
+
+//* PLANIFICACION
+
+// crear caja de juego
+// crear pantalla inicio
+// crear pantalla juego
+// crear pantalla final
+
+// hacer imagen
+
+// pollito (x, y, w, h)
+// enemigos (x, y, w, h)
+
+// el pollito aparece una vez al inicio del juego
+// aparecen enemigos
+
+// colisiones pollito contra enemigos (game over)
+
+// movimiento del pollito
+// movimiento enemigos
+
+// score => por tiempo
+
+
+// *BONUS
+
+// incrementar dificultad (enemigos mas rapidos, mas enemigos)
+// mejoras para el personaje
+// enemigos se mueven hacia jugador
